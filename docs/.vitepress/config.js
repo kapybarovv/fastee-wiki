6cfc1e7000faf5ab8189b093dd818265d1ae95da
root <root@dizzy-azure-partridge.play2go.cloud>
2026-03-26 19:53:54 +0000

    add yandex metrika

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,22 @@
 export default {
   title: 'Fastee Wiki',
   description: 'База знаний',
+  head: [
+    [
+      'script',
+      {},
+      `(function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+      })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=108257267','ym');
+      ym(108257267, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});`
+    ],
+    [
+      'noscript',
+      {},
+      '<div><img src="https://mc.yandex.ru/watch/108257267" style="position:absolute; left:-9999px;" alt="" /></div>'
+    ]
+  ]
 }
